c22a0b13c1329e345eb501e1795e5513b3a948c4
miodraggosic <68286122+miodraggosic@users.noreply.github.com>
2022-11-22 22:02:29 +0100

    api const extracted

---
+++ app/components/env.js
@@ -0,0 +1,6 @@
+const localPort = "http://localhost:3000/";
+
+const apiCandidates = `${localPort} + candidates`;
+const apiCategories = `${localPort} + categories`;
+
+export { localPort, apiCandidates, apiCategories };
