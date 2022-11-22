cc5912a31d2373caa86a0a3635ab9dc9e353a60f
miodraggosic <gosmio88@gmail.com>
2022-11-22 23:16:47 +0100

    api address change

--- app/components/env.js
+++ app/components/env.js
@@ -1,6 +1,7 @@
 const localPort = "http://localhost:3000/";
 
-const apiCandidates = `${localPort} + candidates`;
-const apiCategories = `${localPort} + categories`;
+const apiCandidates = `${localPort}candidates`;
+const apiCategories = `${localPort}categories`;
+console.log(apiCandidates, apiCategories);
 
 export { localPort, apiCandidates, apiCategories };
